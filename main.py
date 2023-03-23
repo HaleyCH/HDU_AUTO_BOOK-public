3099fb2c960eb9f394d9b37f8cff0fdc6d7eb74c
haleyc <1617335718@qq.com>
2023-03-23 12:10:44 +0800

    修改提示信息

--- main.py
+++ main.py
@@ -163,11 +163,11 @@ if __name__ == "__main__":
     if "自习室" not in cfg[key]["type"]:
         # 阅览室
         if datetime.now().hour <= 20 - time_zone or datetime.now().hour == 20 - time_zone and datetime.now().minute < 30:  # github action cron定时有波动
-            print("阅览室预约，21点开始预约")
+            print("阅览室预约于21点开始预约，现在还未到预约时间，请检查下一个Action")
             exit(0)
     else:
         if datetime.now().hour > 20 - time_zone and datetime.now().minute > 30:
-            print("请检查上一个预约")
+            print("自习室已于上个Action预约，请检查上一个预约")
             exit(0)
 
     print("尝试预约,开始时间：{}，持续时间：{}小时".format(cfg[key]['开始时间'], cfg[key]['持续小时数']))
